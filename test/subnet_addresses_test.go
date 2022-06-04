1f2ede75cb07289e569293d82b0fa14f374b80cf
Victor-Laurentiu Stanescu <victor611@yahoo.com>
2022-06-04 15:03:47 +0300

    aws-vpc: add verify job via GitHub Actions

--- test/subnet_addresses_test.go
+++ test/subnet_addresses_test.go
@@ -37,39 +37,39 @@ func TestSubnetAddresses(t *testing.T) {
 	equalsplit_public_subnets := TerraformOutput("equalsplit_public_subnets")
 	equalsplit_power_of_two := TerraformOutput("equalsplit_power_of_two")
 
-	assert.Equal(t, map[string][]string {
-		"private_subnets": []string {"10.0.0.0/17", "10.0.128.0/18", "10.0.192.0/18"},
-		"public_subnets": []string {},
-		"unused_subnets": []string {},
+	assert.Equal(t, map[string][]string{
+		"private_subnets": []string{"10.0.0.0/17", "10.0.128.0/18", "10.0.192.0/18"},
+		"public_subnets":  []string{},
+		"unused_subnets":  []string{},
 	}, nowaste_private_subnets)
 
-	assert.Equal(t, map[string][]string {
-		"private_subnets": []string {"10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19", "10.0.128.0/20", "10.0.144.0/20"},
-		"public_subnets": []string {"10.0.160.0/20", "10.0.176.0/20", "10.0.192.0/20", "10.0.208.0/20", "10.0.224.0/20", "10.0.240.0/20"},
-		"unused_subnets": []string {},
+	assert.Equal(t, map[string][]string{
+		"private_subnets": []string{"10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19", "10.0.128.0/20", "10.0.144.0/20"},
+		"public_subnets":  []string{"10.0.160.0/20", "10.0.176.0/20", "10.0.192.0/20", "10.0.208.0/20", "10.0.224.0/20", "10.0.240.0/20"},
+		"unused_subnets":  []string{},
 	}, nowaste_public_subnets)
 
-	assert.Equal(t, map[string][]string {
-		"private_subnets": []string {"10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19"},
-		"public_subnets": []string {"10.0.128.0/19", "10.0.160.0/19", "10.0.192.0/19", "10.0.224.0/19"},
-		"unused_subnets": []string {},
+	assert.Equal(t, map[string][]string{
+		"private_subnets": []string{"10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19"},
+		"public_subnets":  []string{"10.0.128.0/19", "10.0.160.0/19", "10.0.192.0/19", "10.0.224.0/19"},
+		"unused_subnets":  []string{},
 	}, nowaste_power_of_two)
 
-	assert.Equal(t, map[string][]string {
-		"private_subnets": []string {"10.0.0.0/18", "10.0.64.0/18", "10.0.128.0/18"},
-		"public_subnets": []string {},
-		"unused_subnets": []string {"10.0.192.0/18"},
+	assert.Equal(t, map[string][]string{
+		"private_subnets": []string{"10.0.0.0/18", "10.0.64.0/18", "10.0.128.0/18"},
+		"public_subnets":  []string{},
+		"unused_subnets":  []string{"10.0.192.0/18"},
 	}, equalsplit_private_subnets)
 
-	assert.Equal(t, map[string][]string {
-		"private_subnets": []string {"10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20", "10.0.48.0/20", "10.0.64.0/20", "10.0.80.0/20"},
-		"public_subnets": []string {"10.0.96.0/20", "10.0.112.0/20", "10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20", "10.0.176.0/20"},
-		"unused_subnets": []string {"10.0.192.0/20", "10.0.208.0/20", "10.0.224.0/20", "10.0.240.0/20"},
+	assert.Equal(t, map[string][]string{
+		"private_subnets": []string{"10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20", "10.0.48.0/20", "10.0.64.0/20", "10.0.80.0/20"},
+		"public_subnets":  []string{"10.0.96.0/20", "10.0.112.0/20", "10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20", "10.0.176.0/20"},
+		"unused_subnets":  []string{"10.0.192.0/20", "10.0.208.0/20", "10.0.224.0/20", "10.0.240.0/20"},
 	}, equalsplit_public_subnets)
 
-	assert.Equal(t, map[string][]string {
-		"private_subnets": []string {"10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19"},
-		"public_subnets": []string {"10.0.128.0/19", "10.0.160.0/19", "10.0.192.0/19", "10.0.224.0/19"},
-		"unused_subnets": []string {},
+	assert.Equal(t, map[string][]string{
+		"private_subnets": []string{"10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19"},
+		"public_subnets":  []string{"10.0.128.0/19", "10.0.160.0/19", "10.0.192.0/19", "10.0.224.0/19"},
+		"unused_subnets":  []string{},
 	}, equalsplit_power_of_two)
 }
